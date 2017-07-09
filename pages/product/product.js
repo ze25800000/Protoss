760184c5e711a6069dd5669a63a4f053ca65268d
ze25800000 <ze258100000@sina.com>
2017-07-09 16:54:23 +0800

    商品详情页（2）使用Picker组件修改商品数量

--- pages/product/product.js
+++ pages/product/product.js
@@ -2,19 +2,30 @@ import {Product} from 'product-model';
 var product = new Product();
 Page({
 
-    data: {},
+    data: {
+        id           : null,
+        countsArray  : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        productCounts: 1
+    },
 
-    onLoad   : function (options) {
+    onLoad          : function (options) {
         var id       = options.id;
         this.data.id = id;
         this._loadData();
     },
-    _loadData: function () {
+    _loadData       : function () {
         product.getDetailInfo(this.data.id, data => {
             console.log(data);
             this.setData({
                 product: data
             });
         });
+    },
+    bindPickerChange: function (event) {
+        var index         = event.detail.value;
+        var selectedCount = this.data.countsArray[index];
+        this.setData({
+            productCounts: selectedCount
+        })
     }
 });
