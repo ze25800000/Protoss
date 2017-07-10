b380c8226d56127fbed2554ec99d5e815bc1b328
ze25800000 <ze258100000@sina.com>
2017-07-10 17:31:21 +0800

    以面向对象的方式编写购物车代码

---
+++ pages/cart/cart-model.js
@@ -0,0 +1,65 @@
+import {Base} from '../../utils/base';
+class Cart extends Base {
+    constructor() {
+        super();
+        this._storageKeyName = 'cart';
+    }
+
+    /**
+     * 加入到购物车
+     * 如果之前没有样的商品，则直接添加一条新的记录， 数量为 counts
+     * 如果有，则只将相应数量 + counts
+     * @params:
+     * item - {obj} 商品对象,
+     * counts - {int} 商品数目,
+     * */
+    add(item, counts) {
+        var cartData  = this.getCartDataFromLocal();
+        var isHasInfo = this._isHasThatOne(item.id, cartData);
+        if (isHasInfo.index == -1) {
+            item.count        = counts;
+            item.selectStatus = true; //设置选中状态
+            cartData.push(item);
+        } else {
+            cartData[isHasInfo.index].counts += counts;
+        }
+        wx.setStorageSync(this._storageKeyName, cartData);
+    }
+
+    /**
+     * 从缓存中读取购物车数据
+     * @returns {*}
+     */
+    getCartDataFromLocal() {
+        var res = wx.getStorageSync(this._storageKeyName);
+        if (!res) {
+            res = [];
+        }
+        return res;
+    }
+
+    /**
+     * 判断某个商品是否已经被添加到购物车中，
+     * 并且返回这个商品的数据以及所在数组中的序号
+     * @param id
+     * @param arr
+     * @returns {{index: number}}
+     * @private
+     */
+    _isHasThatOne(id, arr) {
+        var item,
+            result = {index: -1};
+        for (let i = 0; i < arr.length; i++) {
+            item = arr[i];
+            if (item.id == id) {
+                result = {
+                    index: i,
+                    data : item
+                };
+                break;
+            }
+        }
+        return result;
+    }
+}
+export {Cart};
