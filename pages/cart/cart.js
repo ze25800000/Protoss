73eef864715e77b5a6c32e4dde14efe4a9f9c691
ze25800000 <ze258100000@sina.com>
2017-07-11 18:05:08 +0800

    购物车页面（3）自定义CheckBox及状态控制

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -14,7 +14,7 @@ Page({
         var cartData = cart.getCartDataFromLocal();
         var cal      = this._calcTotalAccountAndCounts(cartData);
         this.setData({
-            selectedCount: cal.selectedCounts,
+            selectedCounts: cal.selectedCounts,
             selectedTypeCounts: cal.selectedTypeCounts,
             account: cal.account,
             cartData: cartData
@@ -24,10 +24,8 @@ Page({
         var len                = data.length,
             //需要计算的总价格，排除未选中的商品
             account            = 0,
-
             //购买商品的总个数
             selectedCounts     = 0,
-
             //购买商品种类的总数
             selectedTypeCounts = 0;
         var multiple           = 100;
@@ -43,6 +41,42 @@ Page({
             selectedCounts: selectedCounts,
             selectedTypeCounts: selectedTypeCounts,
             account: account / (multiple * multiple)
+        }
+    },
+    _resetCartData(){
+        //重新计算总金额和商品总数
+        var newData = this._calcTotalAccountAndCounts(this.data.cartData);
+        this.setData({
+            account: newData.account,
+            selectedCounts: newData.selectedCounts,
+            selectedTypeCounts: newData.selectedTypeCounts,
+            cartData: this.data.cartData
+        })
+    },
+    toggleSelect(event){
+        var id     = cart.getDataSet(event, 'id'),
+            status = cart.getDataSet(event, 'status'),
+            index  = this._getProductIndexById(id);
+
+        this.data.cartData[index].selectStatus = !status;
+        this._resetCartData();
+    },
+    toggleSelectAll(event){
+
+    },
+    /**
+     * 根据商品id获取商品数组所在下标
+     * @param id
+     * @returns {number}
+     * @private
+     */
+    _getProductIndexById(id){
+        var data = this.data.cartData,
+            len  = data.length;
+        for (let i = 0; i < len; i++) {
+            if (data[i].id == id) {
+                return i;
+            }
         }
     }
 });
